335249b46f1be9410c39e9b0c36a1e313671d68d
Mateusz Makowski <matmakos@gmail.com>
2018-11-29 13:53:12 +0100

    Add body-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 
@@ -19,6 +20,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+app.use(bodyParser.json());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
